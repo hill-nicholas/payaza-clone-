d31bae78f458b5b1c1a8e42191739b13a9767dc7
hillary nicholas <75504607+hill-nicholas@users.noreply.github.com>
2025-03-14 16:02:51 +0100

    finished part
    finally bro , i finished with the project

--- src/Sections/Footer/index.tsx
+++ src/Sections/Footer/index.tsx
@@ -21,70 +21,69 @@ const Footer = () => {
 
         <img src={images.FooterImg} alt="" className="w-[95%]" />
 
+        <div className="flex flex-row gap-5 w-full lg:justify-between">
 
-        <div className="md:flex-row flex-col gap-5 w-full lg:justify-between">
+{/* Left Section */}
+<div className="w-[95%] md:w-[60%] lg:w-[50%] flex md:flex-row flex-wrap lg:flex-nowrap md:justify-between">
 
+  <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
+    <p className="font-semibold">Products</p>
+    <MdOutlineKeyboardArrowRight className="md:hidden" size={25} />
+    <div className="md:flex hidden flex-col gap-1">
+      <p>Payout</p>
+      <p>Collections</p>
+      <p>Checkout Demo</p>
+    </div>
+  </div>
 
-            <div className="w-[95%] md:w-[95%] lg:w-[50%] flex md:flex-row flex-wrap lg:flex-nowrap md:justify-between">
+  <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
+    <p className="font-semibold">Company</p>
+    <MdOutlineKeyboardArrowRight className="md:hidden" size={25}/>
+    <div className="md:flex hidden flex-col gap-1">
+      <p>About Us</p>
+      <p>Pricing</p>
+    </div>
+  </div>
 
+  <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
+    <p className="font-semibold">Learn & Resources</p>
+    <MdOutlineKeyboardArrowRight className="md:hidden" size={25} />
+    <div className="md:flex flex-col gap-1 hidden">
+      <p>Support</p>
+      <p>Blog</p>
+      <p>Developers</p>
+      <p>Status</p>
+      <p>FAQs</p>
+    </div>
+  </div>
 
-                <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
-                    <p className="font-semibold">Products</p>
-                    <MdOutlineKeyboardArrowRight className="md:hidden" size={25} />
-                    <div className="md:flex hidden flex-col gap-1">
-                        <p>Payout</p>
-                        <p>Collections</p>
-                        <p>Checkout Demo</p>
-                    </div>
-                </div>
+  <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
+    <p className="font-semibold">Security</p>
+    <MdOutlineKeyboardArrowRight className="md:hidden" size={25}/>
+    <div className="md:flex flex-col gap-1 hidden">
+      <p>Terms and Conditions</p>
+      <p>Privacy Policy</p>
+      <p>ESG Policy</p>
+    </div>
+  </div>
 
+</div>
 
+{/* Right Section */}
+<div className="rounded-[16px] bg-[rgb(27,0,41)] px-6 py-8 w-[95%] lg:w-[45%] h-max">
+  <p className="text-sm leading-[16px] font-normal font-roobert text-[rgb(255,255,255)]">
+    Join our exclusive newsletter community and receive our latest updates, news and exclusive offers!
+  </p>
+  <div className="mt-5 flex flex-col lg:flex-row gap-5 items-center justify-between">
+    <input type="text" placeholder="Email address here" className="border border-[#D0D5DD] placeholder:text-[rgb(255,255,255)] text-[rgb(255,255,255)] py-3 px-4 rounded-[104px] w-full lg:w-[80%]" />
+    <button className="text-center px-[14px] py-3 bg-[rgb(68,0,102)] flex items-center gap-2 rounded-[96px] text-white font-medium lg:w-max w-full text-sm mt-5 lg:mt-0">
+      Subscribe
+    </button>
+  </div>
+</div>
 
-                <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
-                    <p className="font-semibold">Company</p>
-                    <MdOutlineKeyboardArrowRight className="md:hidden" size={25}/>
-                    <div className="md:flex hidden flex-col gap-1">
-                        <p>About Us</p>
-                        <p>Pricing</p>
-                    </div>
-                </div>
+</div>
 
-
-                <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
-                    <p className="font-semibold">Learn & Resources</p>
-                    <MdOutlineKeyboardArrowRight className="md:hidden" size={25} />
-                    <div className="md:flex flex-col gap-1 hidden">
-                        <p>Support</p>
-                        <p>Blog</p>
-                        <p>Developers</p>
-                        <p>Status</p>
-                        <p>FAQs</p>
-                    </div>
-                </div>
-
-
-                <div className="flex md:flex-col justify-between md:justify-start md:items-start items-center py-4 gap-1">
-                    <p className="font-semibold">Security</p>
-                    <MdOutlineKeyboardArrowRight className="md:hidden" size={25}/>
-                    <div className="md:flex flex-col gap-1 hidden ">
-                        <p>Terms and Conditions</p>
-                        <p>Privacy Policy</p>
-                        <p>ESG Policy</p>
-                    </div>
-                </div>
-
-
-            </div>
-
-	        <div className="rounded-[16px] bg-[rgb(27,0,41)] px-6 py-8 w-[95%] mt-10 lg:w-[45%] h-max">
-                <p className="text-sm leading-[16px] font-normal font-roobert text-[rgb(255,255,255)]">Join our exclusive newsletter community and receive our latest updates, news and exclusive offers!</p>
-                <div className="mt-5 flex-col lg:flex-row gap-5 items-center justify-between">
-                    <input type="text" placeholder="Email address here" className="border border-[#D0D5DD] placeholder:text-[rgb(255,255,255)] text-[rgb(255,255,255)] py-3 px-4 rounded-[104px] w-full lg:w-[80%]" />
-                    <button className=" text-center px-[14px] py-3 bg-[rgb(68,0,102)] flex items-center gap-2 rounded-[96px] text-white font-medium lg:w-max w-full text-sm mt-5 lg:mt-0  ">Subscribe</button>
-                </div>
-            </div>
-
-        </div>
 
 
         <hr className="w-full text-gray-200" />
--- src/Sections/Navbar/index.tsx
+++ src/Sections/Navbar/index.tsx
@@ -1,16 +1,26 @@
-
-import { RiArrowDropDownLine } from "react-icons/ri"
-import { images } from "../../images"
-import { SlArrowDown } from "react-icons/sl"
+import { useState } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { images } from "../../images";
+import { SlArrowDown } from "react-icons/sl";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="w-full flex items-center justify-center fixed top-0 lg:top-14 z-50">
       <div className="lg:bg-white/50 backdrop-blur-lg lg:w-[87%] w-full flex items-center justify-between p-4 px-5 lg:rounded-[72px] font-roobert border border-white/20">
         <img src={images.PayazaLogo} alt="" className="w-[95px] lg:w-auto" />
 
-        <GiHamburgerMenu className="text-[rgb(68,0,102)] lg:hidden"  size={30}/>
+        {/* Burger Menu Button */}
+        <GiHamburgerMenu
+          className="text-[rgb(68,0,102)] lg:hidden cursor-pointer"
+          size={30}
+          onClick={() => setIsOpen(true)}
+        />
 
         <div className="hidden lg:flex items-center gap-72">
           <div className="flex text-sm items-center gap-5">
@@ -41,10 +51,58 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <motion.div
+          initial={{ y: "-100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-100%" }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          className="fixed top-0 left-0 w-full h-screen bg-white z-50 flex flex-col gap-10 p-4"
+        >
+          {/* Close Button */}
+        <div className="flex gap-[35%] md:gap-[65%] items-center ">
+          <img src={images.PayazaLogo} alt="" className="w-[95px] lg:w-auto" />
+          <div className="flex items-center gap-2 border-2 border-[#98a2b3] rounded-[96px] py-[10px] px-[12px]">
+              <img src={images.NigerianFlag} alt="" className="rounded-full w-5 h-5" />
+              <p className="text-gray-500">NG</p>
+              <SlArrowDown fill="gray" size={10} />
+            </div>
+
+          <IoClose
+              className="absolute top-5 right-5 text-[rgb(68,0,102)] cursor-pointer"
+              size={35}
+              onClick={() => setIsOpen(false)}
+            />
+         </div>
+
+          <div className="flex flex-col gap-10">
+            <div className="flex justify-between items-center">
+                <p className="font-semibold">Product Suite</p>
+                <MdOutlineKeyboardArrowRight className="md:hidden" size={25}/>
+            </div>
+            <div className="flex justify-between items-center">
+                <p className="font-semibold">Resources</p>
+                <MdOutlineKeyboardArrowRight className="md:hidden" size={25}/>
+            </div>
+            <p className="font-semibold">Pricing</p>
+            <p className="font-semibold">Developers</p>
+            <p className="font-semibold">Support</p>
+          </div>
+
+          <div className="flex flex-col mt-40 w-full">
+            <button className=" text-center mb-4 bg-[rgb(68,0,102)] flex items-center justify-center gap-2 rounded-[96px] px-[14px] py-3 text-white font-medium w-full  text-sm">
+              Login
+            </button>
+            <button className="flex items-center justify-center gap-2 rounded-[96px] px-[14px] py-3 font-medium bg-transparent text-black-100 border border-black-100 text-sm w-full text-center">
+              Sign Up
+            </button>
+          </div>
+
+        </motion.div>
+      )}
     </div>
   );
 };
 
 export default Navbar;
-
-
